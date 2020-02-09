1d966ba94959269531c81389ac43c78ef8373c70
Akhil <Akhil@AKHIL.home>
2020-02-09 10:58:07 +0000

    moved to test and deleted from main

--- src/main/java/algos/memoryleak/ObjectWithNoHashCode/ObjectWithNoHashCodeMemoryLeak.java
+++ src/main/java/algos/memoryleak/ObjectWithNoHashCode/ObjectWithNoHashCodeMemoryLeak.java
@@ -1,11 +1,11 @@
 package algos.memoryleak.ObjectWithNoHashCode;
 
 import java.util.Map;
-import org.junit.Test;
+
 
 public class ObjectWithNoHashCodeMemoryLeak {
 
-  @Test(expected = OutOfMemoryError.class)
+//  @Test(expected = OutOfMemoryError.class)
   public void testObjectWithNoHashCodeAndEquals() {
     Map<Object, Object> envMap = System.getProperties();
     while (true) {
