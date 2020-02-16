7474561e58fe8ef78e311938c5272facb89dad75
Akhil <Akhil@AKHIL.home>
2020-02-16 22:32:59 +0000

    added Stackwalker code in java 9

--- src/main/java/java9/StackWalkerClient.java
+++ src/main/java/java9/StackWalkerClient.java
@@ -1,9 +1,29 @@
 package java9;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StackWalkerClient {
+	static StackWalker stackWalkerSpecific;
+	static List<Integer> lines;
 
 	public static void main(String[] args) {
+		//Showing forEach --> to show all stack traces
 		method1();
+		System.out.println("***************************");
+		//Showing Walk to show restricted stackTrace
+		stackWalkerSpecific  = StackWalker.getInstance();
+		method6();
+	}
+	
+	static void method6() {
+		method5();		
+	}
+	
+	static void method5() {
+		lines = stackWalkerSpecific.walk(stackFrameStream-> stackFrameStream.filter(stackFrame->stackFrame.getMethodName().contains("method6"))
+				.map(stackFrame->stackFrame.getLineNumber()).collect(Collectors.toList()));
+		lines.forEach(System.out::println);
 	}
 	
 	static void method4() {
@@ -13,6 +33,7 @@ public class StackWalkerClient {
 	
 	static void method3() {
 		method4();
+		
 	}
 	
 	static void method2() {
